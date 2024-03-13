1bc94689c36c318ee0cd1450beecbfc4ae1799f3
Xiaoan6666 <94225599+Xiaoan6666@users.noreply.github.com>
2024-03-13 08:21:50 +0800

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -15,7 +15,7 @@ RunLog1="/root/Miner/Run.log"
 RunLog2="/root/Miner/Run1.log"
 
 pkill -9 minerARM 2>/dev/null
-pkill -9 minerX86 2>/dev/null
+pkill -9 xdagminer 2>/dev/null
 pkill -9 nbminer 2>/dev/null
 rm -rf $RunLog1 2>/dev/null
 rm -rf $RunLog2 2>/dev/null
@@ -34,8 +34,10 @@ if [ "$ARCH" = "x86_64" ]; then
       miner_speed=$(tail -n 10 Run.log | grep "miner    speed" | tail -n 1)
       RVN_speed=$(tail -n 10 Run1.log | grep "kawpow - On Pool" | tail -n 1)
       RVN_accepted=$(tail -n 10 Run1.log | grep "Share accepted" | tail -n 1)
+      RVN_job=$(tail -n 10 Run1.log | grep "kawpow - New job:" | tail -n 1)
       echo -e "\e[1;32mMiner_X86　　$CORES　$cpu_accepted\e[0m"
       echo -e "\e[1;32mMiner_X86　　$CORES　$miner_speed\e[0m"
+      echo -e "\033[36mNBMiner　　　$CORES　$RVN_job\033[0m"
       echo -e "\033[36mNBMiner　　　$CORES　$RVN_speed\033[0m"
       echo -e "\033[36mNBMiner　　　$CORES　$RVN_accepted\033[0m"
       sleep 1
